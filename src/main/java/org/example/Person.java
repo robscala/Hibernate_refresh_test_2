1540b951019112ddf2ff6daaf20dd39cd92f93d6
Rob Scala <rob.scala@buyersedgeplatform.com>
2023-03-28 17:00:02 -0400

    Add the remaining files.

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
+
+@Entity
+@Table(name="person")
+public class Person
+{
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long id;
+
+    private String firstName;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name = "COMPANY_ID", foreignKey = @ForeignKey())
+    private Company company;
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+}
---
+++ src/test/java/org/example/ManufacturerTest.java
@@ -0,0 +1,52 @@
+package org.example;
+
+import java.util.List;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class ManufacturerTest
+{
+    private static final long MANUFACTURER_ID = 364340L;
+    private static final long PERSON_ID = 14532L;
+
+    private EntityManagerFactory entityManagerFactory;
+
+    @Before
+    public void init() {
+        entityManagerFactory = Persistence.createEntityManagerFactory( "refresh_test" );
+    }
+
+    @After
+    public void destroy() {
+        entityManagerFactory.close();
+    }
+
+    @Test
+    public void refresh_test() throws Exception {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        Manufacturer manufacturer = entityManager.find(Manufacturer.class, MANUFACTURER_ID);
+        System.out.println("Refreshing manufacturer");
+        entityManager.refresh(manufacturer);
+        System.out.println("Calling manufacturer.getPeople()");
+        List<Person> people = manufacturer.getPeople();
+        Person person1 = people.get(0);
+        System.out.println("Changing name");
+        String newFirstName = "name1".equals(person1.getFirstName()) ? "name2" : "name1";
+        entityManager.getTransaction().begin();
+        person1.setFirstName(newFirstName);
+        entityManager.getTransaction().commit();
+
+        assertEquals("Get latest name", newFirstName, person1.getFirstName());
+
+        entityManager.close();
+    }
+}
